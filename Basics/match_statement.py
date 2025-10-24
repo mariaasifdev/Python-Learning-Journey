b494400ce864dbc02d8dd9e22ac9680e65c0275a
mariaasifdev <mariaasif190@gmail.com>
2025-10-24 20:09:41 +0500

    Updated match_statement

---
+++ Basics/match_statement.py
@@ -0,0 +1,58 @@
+# The match statement is used to perform different actions based on different conditions. Instead of using many if-else statements, we can use this: 
+# Syntax:
+# match expression:
+#   case x:
+#     code block
+#   case y:
+#     code block
+#   case z:
+#     code block
+
+
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+
+# we can use underscore _, if we want to execute a set of code, when the condition didn't met:
+day = 4
+match day:
+  case 6:
+    print("Today is Saturday")
+  case 7:
+    print("Today is Sunday")
+  case _:
+    print("Looking forward to the Weekend")
+
+# combine value using (pipe |) character, if we wanted to check more than one case at a time 
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5:
+    print("Today is a weekday")
+  case 6 | 7:
+    print("I love weekends!")
+
+# If Statements as Guards --> we can add if statements in the case evaluation as an extra condition-check:
+month = 5
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5 if month == 4:
+    print("A weekday in April")
+  case 1 | 2 | 3 | 4 | 5 if month == 5:
+    print("A weekday in May")
+  case _:
+    print("No match")
+
+# Match statements (introduced in Python 3.10) are NOT essential - just having a concept is usually enough.
